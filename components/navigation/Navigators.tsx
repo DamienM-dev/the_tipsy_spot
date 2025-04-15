626795c37254a696cae7bce1010c6c06b300b583
Damien <damien.miremont@gmail.com>
2025-04-15 12:01:57 +0200

    custom headers

--- components/navigation/Navigators.tsx
+++ components/navigation/Navigators.tsx
@@ -6,14 +6,33 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import HomeScreen from '../home/Home';
 import ListProducts from '../products/ListProducts';
+import { Colors } from '@/constants/Colors';
 
+
+// NAVIGATION
 const StackNavigatorContainer = createStackNavigator();
+const styleNavigation = {
+    headerBackTitleStyle:{
+        fontFamily:"textFonts"
+},
+    headerTintColor:Colors.textColor.black,
+    headerStyle:{
+        borderBottomColor: Colors.secondary.red,
+        borderBottomWidth:1
+    }
+ 
+}
 
 const StackNavigator = () => {
     return(
         <StackNavigatorContainer.Navigator>
-            <StackNavigatorContainer.Screen name='Home' component={HomeScreen} />
-            <StackNavigatorContainer.Screen name='ListProducts' component={ListProducts} />
+            <StackNavigatorContainer.Screen name='Home' component={HomeScreen} options={
+                {
+                    title:"Accueil",
+                    ...styleNavigation
+                   
+            }} />
+            <StackNavigatorContainer.Screen name='ListProducts' component={ListProducts} options={{title:"Cocktails", ...styleNavigation}} />
         </StackNavigatorContainer.Navigator>
     )
 }
---
+++ components/products/IconesTop.tsx
@@ -0,0 +1,21 @@
+import { Image, Text, View } from "react-native"
+
+// TYPE
+
+type iconeTop = {
+    image:string,
+    name:string
+}
+
+const IconesTop = () => {
+    return(
+        <View>
+            <View>
+                <Image />
+                <Text></Text>
+            </View>
+        </View>
+    )
+}
+
+export default IconesTop;
