f6157b242e5ba1c08840310e07d24b3a3a88d5b6
Damien <damien.miremont@gmail.com>
2025-05-05 11:27:25 +0200

    debut style de la page d'un cocktail

--- components/products/product/HomeProductsView.tsx
+++ components/products/product/HomeProductsView.tsx
@@ -1,9 +1,23 @@
 import { Colors } from "@/constants/Colors"
-import { ScrollView, StyleSheet, View } from "react-native"
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native"
+
+// TYPE
+
+const title:string = "Negroni"
 
 const HomeProductView = () => {
     return(
         <ScrollView style={styles.containerGeneralHome}>
+            <View style={styles.blockImage}>
+                <Image 
+                source={require('../../../assets/images/negroni.jpg')}
+                style={styles.imageStyle}
+               />
+            </View>
+            <View>
+                <Text style={styles.titleStyle}>{title}</Text>
+            </View>
+
             
         </ScrollView>
     )
@@ -12,7 +26,22 @@ const HomeProductView = () => {
 const styles = StyleSheet.create({
     containerGeneralHome: {
         backgroundColor: Colors.primary.beige
+    },
+    blockImage: {
+        width:"100%",
+    
+    },
+    imageStyle: {
+        width:"100%",
+        height:400
+    },
+    titleStyle: {
+        fontSize:30,
+        fontFamily:'SpaceGrotesk-Regular',
+        fontWeight:"bold"
     }
 })
+
+
 
 export default HomeProductView
